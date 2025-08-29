be9e4d8d72c1764de40ee24dc1d38a4c9f28dc5a
Samuel Schneweiss <schneweisscavalcanti@gmail.com>
2025-08-29 17:51:00 -0300

    DB connection added

---
+++ src/br/com/spotifyProject/services/DBConnection.java
@@ -0,0 +1,20 @@
+package br.com.spotifyProject.services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/spotify_project";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar no banco: " + e.getMessage());
+            throw e;
+        }
+    }
+}
